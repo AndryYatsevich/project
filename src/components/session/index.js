977923c01cb6dfdb17b8b97b03ba799b0e20f2d3
Andrey Yatsevich <yatsevichao@gmail.com>
2018-03-02 13:38:29 +0400

    4 commit

--- src/components/session/index.js
+++ src/components/session/index.js
@@ -5,8 +5,8 @@ import {loadSessionItem} from './action';
 import {PanelGroup, Panel} from 'react-bootstrap';
 
 class Session extends React.Component {
-    constructor() {
-        super();
+    constructor(props, context) {
+        super(props, context);
 
         this.handleSelect = this.handleSelect.bind(this);
         this.state = {
@@ -23,47 +23,56 @@ class Session extends React.Component {
     }
 
     click() {
-        console.log(this.props.sessionItem);
         return this.props.sessionItem.map((el) => {
             return console.log(el);
         })
     }
 
     render() {
-        let session = <div>Nu takoe</div>;
         return (
-            <div>{session}
-                <div className={'takoe'}>{setTimeout(function () {
-                   return this.props.sessionItem.map((el) => { return <div>{el.title} {console.log(el)}</div>
-                })
-                }.bind(this), 50)
-                }</div>
+            <div>
+                <PanelGroup
+                    accordion
+                    id="accordion-controlled-example"
+                    activeKey={this.state.activeKey}
+                    onSelect={this.handleSelect}
+                >
+                    {setTimeout( function()  {
+                        this.props.sessionItem.map((el) => {
+                        return (<Panel eventKey={el.id}>
+                            {console.log(el)}
+                            <Panel.Heading>
+                                <Panel.Title toggle>{el.title}</Panel.Title>
+                            </Panel.Heading>
+                            <Panel.Body collapsible>Panel content 2
+                                <button onClick={() => this.click()}>Console.log</button>
+                            </Panel.Body>
+
+                        </Panel>)
+                    })}.bind(this), 1000)}
+
+
+                    <Panel eventKey="2">
+                        <Panel.Heading>
+                            <Panel.Title toggle>Panel heading 2</Panel.Title>
+                        </Panel.Heading>
+                        <Panel.Body collapsible>Panel content 2
+                            <button onClick={() => this.click()}>Console.log</button>
+                        </Panel.Body>
+
+                    </Panel>
+                    <Panel eventKey="3">
+                        <Panel.Heading>
+                            <Panel.Title toggle>Panel heading 2</Panel.Title>
+                        </Panel.Heading>
+                        <Panel.Body collapsible>Panel content 2
+                            <button onClick={() => this.click()}>Console.log</button>
+                        </Panel.Body>
+
+                    </Panel>
+
+                </PanelGroup>
             </div>
-
-
-            /*<PanelGroup
-                accordion
-                id="accordion-controlled-example"
-                activeKey={this.state.activeKey}
-                onSelect={this.handleSelect}
-            >
-                {console.log('sessionItem:', this.props.sessionItem)}
-
-
-
-
-
-                <Panel eventKey="2">
-                    <Panel.Heading>
-                        <Panel.Title toggle>Panel heading 2</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body collapsible>Panel content 2
-                        <button onClick={() => this.click()}>Console.log</button>
-                    </Panel.Body>
-
-                </Panel>
-
-            </PanelGroup>*/
         )
 
     }
